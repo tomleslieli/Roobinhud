ece8761c3f1b44962bbcff9508924a2c8e69bab5
Tom Li <tomleslieli@yahoo.com>
2022-06-22 18:03:33 -0400

    Z-index working, chart slightly styled

--- frontend/components/chart/chart.jsx
+++ frontend/components/chart/chart.jsx
@@ -70,7 +70,7 @@ const MyChart = () => {
                             }
                         },
                         tooltip: {
-                            // enabled: false,
+                            enabled: false,
                             mode: 'index',
                             intersect: false
                         },
